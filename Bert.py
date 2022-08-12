b0a3dcbece73a7676be54ec643d5a314919dc5e5
Gabe Sankar <59175411+GabeSankar@users.noreply.github.com>
2022-08-12 00:52:49 -0400

    Update Bert.py
    debugged tokenizer

--- Bert.py
+++ Bert.py
@@ -17,48 +17,62 @@ class Bert:
         #might remove
         # del self.model
         # del self.trainer
-
-        self.DatasetReparser("OldDatasets/train.json", "train.json")
+        #self.DatasetReparser("OldDatasets/train.json", "train.json")
         print("done reparsing train.json")
-        self.DatasetReparser("OldDatasets/test.json", "test.json")
+        #self.DatasetReparser("OldDatasets/test.json", "test.json")
         print("done reparsing test.json")
         torch.cuda.empty_cache()
         os.environ["TOKENIZERS_PARALLELISM"] = "false"
         self.configuration = BertConfig()
 
         self.model = BertModel(self.configuration)
-
-        self.tokenizer = BertTokenizer(vocab_file=vocabFile, do_lower_case=onlyLowercase)
-        #self.tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-
+        self.model.to(torch.device("cpu"))
+        #self.tokenizer = BertTokenizer( do_lower_case=onlyLowercase)
+        self.tokenizer = AutoTokenizer.from_pretrained("bert-large-cased")
+        data_collator = DataCollatorWithPadding(self.tokenizer)
         datasetFiles = {"train": "train.json", "test": "test.json"}
         dataset = load_dataset("json", data_files=datasetFiles)
         print(dataset)
-        tokenized_dataset = dataset.map(self.tokenizer_function, batched=True)
+        #tokenized_dataset_train = pa.table({})
+        #tokenized_dataset_test = pa.table({})
+    
+        #for element in dataset["train"]:
+            # print(type(element["text"]))    #might get index["input_ids"]
+           # dataset["train"][i]["text"] = self.tokenizer_function(element["text"])
+           # dataset["train"][i]["input"] = self.tokenizer_function(element["input"])
+           # train_bar.update(1)
+           # i=i+1
+        #i=0
+        #for element in dataset["test"]:
+          #  dataset["test"][i]["text"] = self.tokenizer_function(element["text"])
+           # dataset["test"][i]["input"] = self.tokenizer_function(element["input"])
+            #i=i+1
 
+        tokenized_dataset = dataset.map(self.tokenizer_function,batched=True)
 
-        train_args = TrainingArguments(output_dir="Trainer", evaluation_strategy="epoch")
+        train_args = TrainingArguments(output_dir="Trainer", evaluation_strategy="epoch",learning_rate=2e-5,weight_decay=0.01)
 
         self.metric = load_metric("accuracy")
-
+                                                                  
         #self.trainer = Trainer(model=self.model, args=train_args, train_dataset=train_dataset, eval_dataset=test_dataset, compute_metrics=self.metric)
-        #tokenized_dataset.remove_column(["input"])
-        #tokenized_dataset.rename_column("text", "labels")
+        #tokenized_dataset.remove_columns(["input","text","input_ids","token_type_ids","attention_mask"])
         #tokenized_dataset.set_format("torch")
-        print(tokenized_dataset)
-        small_train_dataset = tokenized_dataset["train"].shuffle(seed=42).select(range(1000))
-        small_test_dataset = tokenized_dataset["test"].shuffle(seed=42).select(range(1000))
+        small_train_dataset = tokenized_dataset["train"].shuffle(seed=42)
+        small_test_dataset = tokenized_dataset["test"].shuffle(seed=42)
+        self.trainer = Trainer(model=self.model,args=train_args,train_dataset=small_train_dataset,eval_dataset=small_test_dataset,compute_metrics=self.metric,data_collator=data_collator)
+        print(small_train_dataset)
+        self.train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=8,collate_fn=data_collator)
 
-        self.train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=8)
-
-        self.eval_dataloader = DataLoader(small_test_dataset, batch_size=8)
+        self.eval_dataloader = DataLoader(small_test_dataset, batch_size=8,collate_fn=data_collator)
 
         if torch.cuda.is_available():
             self.device = torch.device("cuda")
         else:
             self.device = torch.device("cpu")
+        print(torch.cuda.is_available())
     def train(self, num_epochs):
-
+        self.trainer.train()
+    """
         num_training_steps = num_epochs * len(self.train_dataloader)
         optimizer = AdamW(self.model.parameters(), lr=5e-5)
         lr_scheduler = get_scheduler(name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
@@ -68,7 +82,7 @@ class Bert:
         self.model.train()
         for epoch in range(num_epochs):
             for batch in self.train_dataloader:
-                batch = {k: v.to(self.device) for k,v in batch.items}
+                batch = {k: v.to(self.device) for k,v in batch.items()}
                 outputs = self.model(**batch)
                 loss = outputs.loss
                 loss.backward()
@@ -77,7 +91,7 @@ class Bert:
                 lr_scheduler.step()
                 optimizer.zero_grad()
                 progress_bar.update(1)
-
+    """
     def test(self):
         self.model.eval()
         for batch in self.eval_dataloader:
@@ -92,12 +106,24 @@ class Bert:
         self.metric.compute()
 
     def tokenizer_function(self, example):
-        return self.tokenizer(example["input"], example["text"], truncation=True)
+        #tokens = self.tokenizer.tokenize(example)
+        #ids = self.tokenizer.convert_tokens_to_ids(tokens)
+        #final_inputs = self.tokenizer.prepare_for_model(ids)
+        return self.tokenizer(example["input"],example["text"],truncation=True,padding=True,max_length=512)
 
     #Note that this is only for wiki bio
     def DatasetReparser(self, originalFileDir, NewFileDir):
         of = open(originalFileDir)
         nf = open(NewFileDir, 'w',encoding="utf-8")
+        with of as handle:
+            json_data = [json.loads(line) for line in handle]
+            print("done w/ json data")
+
+        for unfilteredData in json_data:
+        of = open(originalFileDir)
+        nf = open(NewFileDir, 'w',encoding="utf-8")
+        nf.seek(0)
+        nf.truncate()
         with of as handle:
             json_data = [json.loads(line) for line in handle]
             print("done w/ json data")
@@ -113,7 +139,6 @@ class Bert:
             SecTitle = "section title: " + ' - '.join(unfilteredData["sec_title"]) + " | "
             text = json.dumps(unfilteredData["text"])
             finalInput = docTitle + docTitleBPE + SecTitle + data
-            finalLine= "{\"input\":"+ json.dumps(finalInput) + ", " + "\"text\":" + text +"}\n"
+            finalLine= "{\"input\":"+ json.dumps(finalInput).replace("@@ ","") + ", " + "\"text\":" + text.replace("@@ ","") +"}\n"
             nf.write(finalLine)
-
-
+        #nf.truncate(50000)
