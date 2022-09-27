2d06e5f572ae8e3b5211b3806f201ec8ed57d965
Gabe Sankar <59175411+GabeSankar@users.noreply.github.com>
2022-09-27 15:01:39 -0400

    Update Bert.py
    T5

--- Bert.py
+++ Bert.py
@@ -1,5 +1,5 @@
-from transformers import BertModel, BertConfig, BertTokenizer, TrainingArguments, Trainer, get_scheduler, AutoTokenizer
-from datasets import load_dataset, load_metric
+from transformers import GPT2Model, BertConfig, GPT2Tokenizer, TrainingArguments, Trainer, get_scheduler, AutoTokenizer, DataCollatorWithPadding, AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer, DataCollatorForSeq2Seq
+from datasets import load_dataset, load_metric, arrow_dataset, DatasetDict
 from torch.optim import AdamW
 from torch.utils.data import DataLoader
 import os
@@ -15,51 +15,47 @@ class Bert:
     # WordPiece is a bool same with use only lowercase should be(true, true) for first test
     def __init__(self, vocabFile, wordPiece, onlyLowercase):
         #might remove
-        # del self.model
+        #del self.model
+        #del tokenized_dataset
         # del self.trainer
         #self.DatasetReparser("OldDatasets/train.json", "train.json")
         print("done reparsing train.json")
-        #self.DatasetReparser("OldDatasets/test.json", "test.json")
+        #self.DatasetReparser("OldDatasets/test.json", "test.json") 
         print("done reparsing test.json")
         torch.cuda.empty_cache()
         os.environ["TOKENIZERS_PARALLELISM"] = "false"
-        self.configuration = BertConfig()
-
-        self.model = BertModel(self.configuration)
+        #self.configuration = BertConfig()
+        """GPT2
+        self.model = GPT2Model.from_pretrained("gpt2")
         self.model.to(torch.device("cpu"))
-        #self.tokenizer = BertTokenizer( do_lower_case=onlyLowercase)
-        self.tokenizer = AutoTokenizer.from_pretrained("bert-large-cased")
-        data_collator = DataCollatorWithPadding(self.tokenizer)
-        datasetFiles = {"train": "train.json", "test": "test.json"}
-        dataset = load_dataset("json", data_files=datasetFiles)
-        print(dataset)
-        #tokenized_dataset_train = pa.table({})
-        #tokenized_dataset_test = pa.table({})
-    
-        #for element in dataset["train"]:
-            # print(type(element["text"]))    #might get index["input_ids"]
-           # dataset["train"][i]["text"] = self.tokenizer_function(element["text"])
-           # dataset["train"][i]["input"] = self.tokenizer_function(element["input"])
-           # train_bar.update(1)
-           # i=i+1
-        #i=0
-        #for element in dataset["test"]:
-          #  dataset["test"][i]["text"] = self.tokenizer_function(element["text"])
-           # dataset["test"][i]["input"] = self.tokenizer_function(element["input"])
-            #i=i+1
+        self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        self.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        """
+        self.tokenizer = AutoTokenizer.from_pretrained("t5-small")
+        self.model = AutoModelForSeq2SeqLM.from_pretrained("t5-small")
+        #Collator below is for gpt2
+        #data_collator = DataCollatorWithPadding(tokenizer=self.tokenizer, return_tensors='pt', max_length=1024, padding='max_length' )
+        #Collator below is for seq2seq
+        data_collator = DataCollatorForSeq2Seq(tokenizer=self.tokenizer,model=self.model)
 
-        tokenized_dataset = dataset.map(self.tokenizer_function,batched=True)
-
-        train_args = TrainingArguments(output_dir="Trainer", evaluation_strategy="epoch",learning_rate=2e-5,weight_decay=0.01)
+        #self.tokenize_and_save_dataset()
+        Tokenized_Dataset = DatasetDict.load_from_disk("Tokenized_datasets")
+        #tokenized_dataset = dataset.map(self.tokenizer_function,batched=True,remove_columns=["input","text"])
+        print(len(Tokenized_Dataset["train"][10000]["attention_mask"]))
+        train_args = Seq2SeqTrainingArguments(output_dir="results", evaluation_strategy="epoch",learning_rate=2e-5,per_device_train_batch_size=16,per_device_eval_batch_size=16, weight_decay=0.01, save_total_limit=3,num_train_epochs=1)
 
         self.metric = load_metric("accuracy")
-                                                                  
-        #self.trainer = Trainer(model=self.model, args=train_args, train_dataset=train_dataset, eval_dataset=test_dataset, compute_metrics=self.metric)
+        #tokenized_dataset = tokenized_dataset.remove_columns(tokenized_dataset["train"].column_names)
+
+
         #tokenized_dataset.remove_columns(["input","text","input_ids","token_type_ids","attention_mask"])
-        #tokenized_dataset.set_format("torch")
-        small_train_dataset = tokenized_dataset["train"].shuffle(seed=42)
-        small_test_dataset = tokenized_dataset["test"].shuffle(seed=42)
-        self.trainer = Trainer(model=self.model,args=train_args,train_dataset=small_train_dataset,eval_dataset=small_test_dataset,compute_metrics=self.metric,data_collator=data_collator)
+
+        small_train_dataset = Tokenized_Dataset["train"].shuffle(seed=42)
+        #small_train_dataset = small_train_dataset.rename_column("labels","label")
+        small_train_dataset.set_format("torch")
+        #print(small_train_dataset[12])
+        small_test_dataset = Tokenized_Dataset["test"].shuffle(seed=42)
+        self.trainer = Seq2SeqTrainer(model=self.model,args=train_args,train_dataset=small_train_dataset,eval_dataset=small_test_dataset,data_collator=data_collator,tokenizer=self.tokenizer)
         print(small_train_dataset)
         self.train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=8,collate_fn=data_collator)
 
@@ -70,9 +66,19 @@ class Bert:
         else:
             self.device = torch.device("cpu")
         print(torch.cuda.is_available())
+    def tokenize_and_save_dataset(self):
+
+        datasetFiles = {"train": "train.json", "test": "test.json"}
+        dataset = load_dataset("json", data_files=datasetFiles)
+        print(dataset)
+        tokenized_dataset = dataset.map(self.tokenizer_function,batched=True)
+
+        #tokenized_dataset = tokenized_dataset.remove_columns(tokenized_dataset["train"].column_names)
+        tokenized_dataset.save_to_disk("Tokenized_datasets")
+
     def train(self, num_epochs):
         self.trainer.train()
-    """
+        """for GPT2 
         num_training_steps = num_epochs * len(self.train_dataloader)
         optimizer = AdamW(self.model.parameters(), lr=5e-5)
         lr_scheduler = get_scheduler(name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
@@ -82,8 +88,14 @@ class Bert:
         self.model.train()
         for epoch in range(num_epochs):
             for batch in self.train_dataloader:
-                batch = {k: v.to(self.device) for k,v in batch.items()}
-                outputs = self.model(**batch)
+                inputs = {k: v.to(self.device) for k, v in batch.items()}
+                labels = {k: v.to(self.device) for k, v in batch.items()}
+                inputs.pop("labels")
+                del labels["input_ids"], labels["attention_mask"]
+                print({k: v.shape for k,v in inputs.items()})
+                print("........................")
+                print({k: v.shape for k,v in labels.items()})
+                outputs = self.model(**inputs)
                 loss = outputs.loss
                 loss.backward()
 
@@ -91,7 +103,7 @@ class Bert:
                 lr_scheduler.step()
                 optimizer.zero_grad()
                 progress_bar.update(1)
-    """
+        """
     def test(self):
         self.model.eval()
         for batch in self.eval_dataloader:
@@ -105,23 +117,29 @@ class Bert:
             self.metric.add_batch(predictions=predictions, references=batch["labels"])
         self.metric.compute()
 
-    def tokenizer_function(self, example):
+     def tokenizer_function(self, example):
+
+        model_inputs = self.tokenizer(example["input"], max_length=128, padding='max_length',truncation=True)
+        with self.tokenizer.as_target_tokenizer():
+            labels = self.tokenizer(example["text"], max_length=128, padding='max_length', truncation=True)
+        #print(type(model_inputs)) 
+        #model_input={'input_ids':model_inputs["input_ids"],'token_type_ids':model_inputs["token_type_ids"],"attention_mask":model_inputs["attention_mask"],"labels":labels["input_ids"]}
+        model_inputs['labels'] = labels["input_ids"]
+        #inputs=example["input"]
+        #targets=example["text"]
+        #model_inputs = self.tokenizer(inputs, text_target=targets,max_length=128, trunctaion=True)
+        return model_inputs
+        #print(type(self.tokenizer(example["input"],example["text"],truncation=True,padding=True,max_length=512)))
+        #return self.tokenizer(example["input"],example["text"],truncation=True,padding=True,max_length=512)
         #tokens = self.tokenizer.tokenize(example)
         #ids = self.tokenizer.convert_tokens_to_ids(tokens)
         #final_inputs = self.tokenizer.prepare_for_model(ids)
-        return self.tokenizer(example["input"],example["text"],truncation=True,padding=True,max_length=512)
 
     #Note that this is only for wiki bio
     def DatasetReparser(self, originalFileDir, NewFileDir):
-        of = open(originalFileDir)
-        nf = open(NewFileDir, 'w',encoding="utf-8")
-        with of as handle:
-            json_data = [json.loads(line) for line in handle]
-            print("done w/ json data")
-
-        for unfilteredData in json_data:
-        of = open(originalFileDir)
-        nf = open(NewFileDir, 'w',encoding="utf-8")
+        print("print")
+        of=open(originalFileDir)
+        nf=open(NewFileDir, 'w',encoding="utf-8")
         nf.seek(0)
         nf.truncate()
         with of as handle:
@@ -142,3 +160,4 @@ class Bert:
             finalLine= "{\"input\":"+ json.dumps(finalInput).replace("@@ ","") + ", " + "\"text\":" + text.replace("@@ ","") +"}\n"
             nf.write(finalLine)
         #nf.truncate(50000)
+                                                                                                                                                                                                 159,9         Bot
